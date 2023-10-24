47d4556d277fa8473a9a5e8cba0e891e1b2acfc8
anoopsapien <anoopsapien@gmail.com>
2023-10-24 23:17:51 +0530

    added test api

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # flask_microservice_non_mvc
+
+
+test api 
+
+http://127.0.0.1:5000/test
--- run.py
+++ run.py
@@ -1,6 +1,6 @@
 from app import create_app
 
 app = create_app()
-
+print("click here: http://127.0.0.1:5000/test")
 if __name__ == "__main__":
-    app.run()
+    app.run(port=5000)
